4d768658d38b0615ed999bf06477033c25463225
Rostyslav Zakharchuk <rostislavzaharcuk@gmail.com>
2026-03-11 14:46:00 +0100

    Fix route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 import { authenticate } from '../middleware/authenticate.js';
 import { updateUserAvatar } from '../controllers/userController.js';
 
-import { upload } from "../middleware/multer.js";
+import { upload } from "./middleware/multer.js";
 
 
 const router = Router();
